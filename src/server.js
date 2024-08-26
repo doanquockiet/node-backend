5b9194748e6d9408c641dd1f065c3801b8093ad7
Doan Quoc Kiet <116879984+doanquockiet@users.noreply.github.com>
2024-08-26 15:10:39 +0700

    UPDATE:: update file env

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+require('dotenv').config();
+
+
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 8888;
+console.log('cehcek env ', port);
+
+const hostname = process.env.HOST_NAME
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/abc', (req, res) => {
+    res.render('example.ejs')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
