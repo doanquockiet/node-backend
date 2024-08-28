3e745a521c9b264ab5fc5fe0f98813f15a1462d3
Doan Quoc Kiet <116879984+doanquockiet@users.noreply.github.com>
2024-08-28 14:17:01 +0700

    UPDATE:: connection database

--- src/server.js
+++ src/server.js
@@ -3,6 +3,27 @@ require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME
+//test connection
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  password: '123456',
+  database: 'hoidanit'
+})
+
+// simple test query
+
+connection.query(
+  'SELECT * FROM Users u ',
+  function (err, result, fields){
+    console.log("check result = ", result);
+    console.log('fielsd =>>>>>',fields);
+    
+  }
+)
 
 const webRouter = require('./routes/web')
 // khai báo router
@@ -11,6 +32,7 @@ app.use('/', webRouter)
 // config teamplate engine
 const configViewEngine = require('./config/viewEnngine')
 configViewEngine(app);
+
 
 
 app.listen(port,hostname, () => {
