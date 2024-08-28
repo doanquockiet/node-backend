f11d990f3fa06c79630d7a939c0fc9013ce2eb6f
Doan Quoc Kiet <116879984+doanquockiet@users.noreply.github.com>
2024-08-28 09:25:37 +0700

    UPDATE:: config static file

--- src/server.js
+++ src/server.js
@@ -8,10 +8,12 @@ const port = process.env.PORT || 8888;
 console.log('cehcek env ', port);
 
 const hostname = process.env.HOST_NAME
-
+// config teamplate engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
+// config static file
+app.use(express.static(path.join(__dirname, 'public')))
+// khai báo router
 app.get('/', (req, res) => {
   res.send('Hello World! with doan quoc kiet')
 })
@@ -20,6 +22,6 @@ app.get('/abc', (req, res) => {
     res.render('example.ejs')
   })
 
-app.listen(port, () => {
+app.listen(port,hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
